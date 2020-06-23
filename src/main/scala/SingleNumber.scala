b78b52f59cd86569675a4027a3930abe778ddbf1
Shivangi Gupta <shivangi1015@gmail.com>
2020-06-23 16:28:22 +0530

    solved leetcode singleNumber problem.

---
+++ src/main/scala/SingleNumber.scala
@@ -0,0 +1,26 @@
+import scala.collection.mutable
+
+/*
+Given a non-empty array of integers, every element appears three times except for one, which appears exactly once.
+Find that single one.
+
+Note:
+
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Example 1:
+
+Input: [2,2,3,2]
+Output: 3
+Example 2:
+
+Input: [0,1,0,1,0,1,99]
+Output: 99
+ */
+object SingleNumber extends App {
+
+  def singleNumber(nums: Array[Int]): Int = {
+    nums.groupBy(identity).filter(values => values._2.length < 3).keys.head
+  }
+  println(singleNumber(Array(2,2,3,2)))
+}
